80429c28bd64f378d3b516ebb99abed2bb2f87a9
shyam264 <132636351+shyam264@users.noreply.github.com>
2023-05-23 18:57:08 +0530

    Add files via upload

---
+++ length of string.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello, world!";
+    int length = strlen(str);
+
+    printf("The length of the string is: %d\n", length);
+
+    return 0;
+}
